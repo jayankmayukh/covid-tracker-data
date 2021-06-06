860ef952454c61c13e1ea7e8043c77dd9bf1e384
Jayank Mayukh <jayank.mayukh@sentieo.com>
2021-06-06 20:02:32 +0530

    Removed functions in favour of static files

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ### `npm start`
-needs netlify to run.
+runs http-server to serve constents of static directory on port 8081.
 
 ### `npm run fetch-data`
-fetches data from [OWID](https://github.com/owid/covid-19-data/tree/master/public/data#license).
+fetches data from [OWID](https://github.com/owid/covid-19-data/tree/master/public/data#license)
+and stores them as static yaml files under static directory.
--- scripts/fetch-data.js
+++ scripts/fetch-data.js
@@ -1,20 +1,67 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
-const papa = require('papaparse');
+const yaml = require('js-yaml');
 const dataUrl = 'https://covid.ourworldindata.org/data/owid-covid-data.json';
-const codeBookUrl = 'https://covid.ourworldindata.org/data/owid-covid-codebook.csv';
 
 fetch(dataUrl)
     .then((resp) => resp.json())
     .then((json) => {
-        fs.writeFileSync(`${__dirname}/../data.json`, JSON.stringify(json, null, 2));
+        Object.entries(json).forEach(([country, { data = [] }]) => {
+            if (data.length === 0 || country.length !== 3) {
+                return;
+            }
+            fs.writeFileSync(
+                `${__dirname}/../static/${country}.yml`,
+                yaml.dump(data)
+            );
+        });
+        return json;
+    })
+    .then((data) => {
+        const nameSource = data;
+        const countryNames = Object.keys(nameSource).filter((country) => country.length === 3);
+        const timeseries = {};
+        const countries = {};
+        countryNames.forEach((country) => {
+            countries[country] = { values: [], timeseries: [], total_cases: -1 };
+            if (data[country]) {
+                Object.keys(data[country]).forEach((key) => {
+                    if (key === 'data') return;
+                    countries[country].values.push(key);
+                });
+            }
+            if (Array.isArray(data[country]?.data)) {
+                data[country].data.forEach((point) => {
+                    Object.keys(point).forEach((key) => {
+                        if (key === 'date') return;
+                        if (key === 'total_cases') {
+                            countries[country].total_cases = point.total_cases;
+                        }
+                        if (!(key in timeseries)) {
+                            timeseries[key] = [country];
+                        } else if (!timeseries[key].includes(country)) {
+                            timeseries[key].push(country);
+                        }
+                        if (!countries[country].timeseries.includes(key)) {
+                            countries[country].timeseries.push(key);
+                        }
+                    });
+                });
+            }
+        });
+        fs.writeFileSync(
+            `${__dirname}/../static/meta.yml`,
+            yaml.dump(
+                {
+                    timeseries,
+                    countries,
+                }
+            )
+        );
+    })
+    .then(() => {
+        fs.writeFileSync(
+            `${__dirname}/../static/lastUpdated.yml`,
+            yaml.dump({ timestamp: new Date().getTime() })
+        );
     });
-
-fetch(codeBookUrl)
-    .then((resp) => resp.text())
-    .then((text) => papa.parse(text, { header: true }).data)
-    .then((json) => {
-        fs.writeFileSync(`${__dirname}/../codebook.json`, JSON.stringify(json, null, 2));
-    });
-
-fs.writeFileSync(`${__dirname}/../lastUpdated.json`, JSON.stringify({ timestamp: (new Date).getTime() }, null, 2));
